0c306a28e1193697b48fc314963f76e788136c2d
Ashford Lee Parmesar <ashfordlee19@gmail.com>
2022-07-29 15:08:32 +0200

    keypad color and animations

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -7,6 +7,7 @@ const useWordle = (solution) => {
     const [guesses, setGuesses] = useState([...Array(6)])
     const [history, setHistory] = useState([])
     const [isCorrect, setIsCorrect] = useState(false)
+    const [usedKeys, setUsedKeys] = useState({})
 
     const formatGuess = () => {
         let solutionArray = [...solution]
@@ -49,6 +50,27 @@ const useWordle = (solution) => {
         setTurn((prevTurn) => {
             return prevTurn + 1
         })
+        setUsedKeys((prevUsedKeys) => {
+            let newKeys = {...prevUsedKeys}
+            
+            formattedGuess.forEach((l) => {
+                const currentColor = newKeys[l.key]
+
+                if (l.color === "green") {
+                    newKeys[l.key] = "green"
+                    return
+                }
+                if (l.color === "yellow" && currentColor !== "green") {
+                    newKeys[l.key] = "yellow"
+                    return
+                }
+                if (l.color ==="grey" && currentColor !=="green" && currentColor !== "yellow") {
+                    newKeys[l.key] = "grey"
+                    return
+                }
+            })
+            return newKeys
+        })
         setCurrentGuess("")
     }
 
@@ -90,7 +112,7 @@ const useWordle = (solution) => {
         }
     }
 
-    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+    return {turn, currentGuess, guesses, isCorrect,usedKeys, handleKeyup}
 }
 
 export default useWordle;
